c2c2765d30d8ca7322029c7b81e8ac5a4b0a6b2a
git@almaz.sh <git@almaz.sh>
2024-06-03 20:54:45 +0000

    isjson and sleep

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export * from "./lib/test";
-export * from "./lib/queque-service";
+// Services
+export { queueService } from './lib/services/queque-service'
+
+// Utilities
+export { sleep } from './lib/utils/sleep'
+
+// is functions
+export { isJson } from './lib/is/isJson'
