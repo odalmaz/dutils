718df75c6b7a71ff94752e2595f5082651e9b2ee
git@almaz.sh <git@almaz.sh>
2024-06-06 18:36:54 +0000

    add turnstile to exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // Services
-export { queueService } from './lib/services/queque-service'
+export { queueService } from './lib/services/queque-service';
 
 // Utilities
-export { sleep } from './lib/utils/sleep'
-export { roundTo } from './lib/utils/round-to'
+export { sleep } from './lib/utils/sleep';
+export { roundTo } from './lib/utils/round-to';
+export { validateTurnstileToken } from './lib/captcha/turnstile';
 
 // is functions
-export { isJson } from './lib/is/is-json'
-export { isFunction } from './lib/is/is-function'
-export { isObject } from './lib/is/is-object'
+export { isJson } from './lib/is/is-json';
+export { isFunction } from './lib/is/is-function';
+export { isObject } from './lib/is/is-object';
